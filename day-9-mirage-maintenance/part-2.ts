7ef76c05b18a348eab3a0503692c96c7c35ad625
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-12-15 20:14:40 +0000

    Mirage Maintenance

---
+++ day-9-mirage-maintenance/part-2.ts
@@ -0,0 +1,19 @@
+// https://adventofcode.com/2023/day/9#part2
+
+function difference(numbers: number[]): number {
+	if (numbers.every((number) => number === 0)) return 0;
+
+	const differences = numbers.slice(1)
+		.map((difference, index) => difference - numbers[index]);
+
+	return numbers[0]! - difference(differences);
+}
+
+const input = await Deno.readTextFile('day-9-mirage-maintenance/input');
+const histories = input.split('\n');
+
+const sum = histories
+	.map((history) => difference(history.split(' ').map(Number)))
+	.reduce((sum, history) => sum + history);
+
+console.log({ sum });
